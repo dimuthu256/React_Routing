89e76ffba06d41f4d430dda68891fca7d6d9ca53
dimuthu256 <dnwnalaka@gmail.com>
2020-05-03 18:55:45 +0800

    react routing single page application

---
+++ src/containers/Blog/Blog.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+// import axios from 'axios';
+
+//import FullPost from '../../components/FullPost/FullPost';
+//import NewPost from './NewPost/NewPost';
+import './Blog.css';
+import { Route, NavLink,Switch, Redirect } from 'react-router-dom'
+import Posts from '../Blog/Posts/Posts'
+import NewPost from '../Blog/NewPost/NewPost'
+
+class Blog extends Component {
+    
+    state = {
+        auth : true
+    }
+
+    render () {
+        return (
+            <div className="Blog"> 
+                <header>
+                    <nav>
+                        <ul>
+                            <li><NavLink to="/posts/" 
+                                exact
+                                activeClassName="my-active"
+                                activeStyle={{
+                                color: '#fa923f',
+                                textDecoration: 'underline'
+                            }}>Posts</NavLink></li>
+
+                            <li><NavLink to={{
+                                pathname: '/new-post',
+                                hash:'#submit',
+                                search:'?quick-submit=true'
+                            }}>New Post</NavLink></li>
+                        </ul>
+                    </nav>
+                </header>  
+                <Switch>
+                    {this.state.auth ? <Route path="/new-post" component= {NewPost} /> : null}
+                    <Route path="/posts"  component= {Posts} />
+                    <Route render={() => <h1>Not Found</h1>} />
+
+                    {/* <Redirect from="/" to="/posts" /> */}
+                </Switch>
+                
+
+            </div>
+        );
+    }
+}
+
+export default Blog;
